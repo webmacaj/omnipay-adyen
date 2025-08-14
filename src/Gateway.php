95eccb1ae24d4987681306cbaceb955c1a32f38f
Jakub Máčaj <webmacaj@gmail.com>
2025-08-14 13:41:50 +0200

    Gateway getters/setters + default parameters

--- src/Gateway.php
+++ src/Gateway.php
@@ -10,4 +10,61 @@ class Gateway extends AbstractGateway
     {
         return 'Adyen';
     }
+
+    public function getDefaultParameters()
+    {
+        return [
+            'testMode' => false
+        ];
+    }
+
+    public function setApiKey($value)
+    {
+        return $this->setParameter('apiKey', $value);
+    }
+
+    public function getApiKey()
+    {
+        return $this->getParameter('apiKey');
+    }
+
+    public function setPrefix($value)
+    {
+        return $this->setParameter('prefix', $value);
+    }
+
+    public function getPrefix()
+    {
+        return $this->getParameter('prefix');
+    }
+
+    public function setMerchantAccount($value)
+    {
+        return $this->setParameter('merchantAccount', $value);
+    }
+
+    public function getMerchantAccount()
+    {
+        return $this->getParameter('merchantAccount');
+    }
+
+    public function setCountryCode($value)
+    {
+        return $this->setParameter('countryCode', $value);
+    }
+
+    public function getCountryCode()
+    {
+        return $this->getParameter('countryCode');
+    }
+
+    public function setLocale($value)
+    {
+        return $this->setParameter('locale', $value);
+    }
+
+    public function getLocale()
+    {
+        return $this->getParameter('locale');
+    }
 }
