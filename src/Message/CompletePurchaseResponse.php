934b2ee632be7e902bb4ad929cc4383522abf5b3
Jakub Máčaj <webmacaj@gmail.com>
2025-08-15 11:56:31 +0200

    Fix complete purchase request/response

--- src/Message/CompletePurchaseResponse.php
+++ src/Message/CompletePurchaseResponse.php
@@ -2,12 +2,11 @@
 
 namespace Omnipay\Adyen\Message;
 
-use Adyen\Model\Checkout\PaymentLinkResponse;
 
 class CompletePurchaseResponse extends PurchaseResponse
 {
     public function isSuccessful()
     {
-        return $this->data['status'] == PaymentLinkResponse::STATUS_PAID;
+        return $this->data['status'] == 'completed';
     }
 }
